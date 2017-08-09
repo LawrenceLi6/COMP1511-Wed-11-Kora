7b49aefe8ff2da19bb43863353ce3c033362598b
Zain Afzal <zain.afz@gmail.com>
2017-08-09 13:31:11 +1000

    added post tute week 3 stuff

--- Week 3/numToWord.c
+++ Week 3/numToWord.c
@@ -1,10 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+char* numToWord(int num);
+int abs(int num);
+
+
 int main(int argc, char* argv[]){
 	// get input
 	int input = -1;
 	scanf("%d",&input);
 	// something you should do
 	// what should we do here?
-
+	if(input < -10 || input > 10){
+		printf("Learn how to code idiot\n");
+	}
 	// magic
+	if(input < 0){
+		printf("negative ");
+	}
+	numToWord(abs(input));
     return EXIT_SUCCESS;
+}
+
+char* numToWord(int num){
+	char* result;
+	if(num == 0){
+		result = "zero";
+	}
+	return "lol";
+}
+
+int abs(int num){
+	if(num < 0){
+		num = -1*num;
+	}	
+	return num;
 }
