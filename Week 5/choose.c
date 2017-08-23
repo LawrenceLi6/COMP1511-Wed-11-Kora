84425c870dc20d9a806468ca2a8ec04b37889522
Zain Afzal <zain.afz@gmail.com>
2017-08-23 21:48:35 +1000

    Added code review stuff

--- Week 5/choose.c
+++ Week 5/choose.c
@@ -34,40 +34,40 @@ int getRandomNumber(){
 int main(int argc, char* argv[]){
     int n = getRandomNumber();
 	if(n == 1){
-		printf("THE LUCKY WINNER IS >>> McKenzie D AND Diane M <<<\n");
+		printf("THE LUCKY WINNER IS >>> Coders || Carlo P And Peter P <<<\n");
 	}
 	if(n == 2){
-		printf("THE LUCKY WINNER IS >>> Carlo P AND Jordan G <<<\n");
+		printf("THE LUCKY WINNER IS >>> Byte Me || Tristan B And Marc R <<<\n");
 	}
 	if(n == 3){
-		printf("THE LUCKY WINNER IS >>> Tristan N AND Yixuan C <<<\n");
+		printf("THE LUCKY WINNER IS >>> lab partner Diane And Rowan || Diane M And Rowan S <<<\n");
 	}
 	if(n == 4){
-		printf("THE LUCKY WINNER IS >>> Peter P AND Lavan S <<<\n");
+		printf("THE LUCKY WINNER IS >>> Spiderman || McKenzie D And Jemima S <<<\n");
 	}
 	if(n == 5){
-		printf("THE LUCKY WINNER IS >>> Noah K AND Louis Y <<<\n");
+		printf("THE LUCKY WINNER IS >>> Tristan Nguyen-dang and Haseeb Syed || Tristan N.D and Haseeb S <<<\n");
 	}
 	if(n == 6){
-		printf("THE LUCKY WINNER IS >>> Yangyuan L AND Andrew M <<<\n");
+		printf("THE LUCKY WINNER IS >>> Arthritic Capsicum || Lavan S and Jordan G <<<\n");
 	}
 	if(n == 7){
-		printf("THE LUCKY WINNER IS >>> Brendan D AND Jiaying Y <<<\n");
+		printf("THE LUCKY WINNER IS >>> What || Bathara L And Yangyuan L <<<\n");
 	}
 	if(n == 8){
-		printf("THE LUCKY WINNER IS >>> Hannah E AND Jemima S <<<\n");
+		printf("THE LUCKY WINNER IS >>> Lab Group || Hannah E And Noah K <<<\n");
 	}
 	if(n == 9){
-		printf("THE LUCKY WINNER IS >>> Bathara L AND Patrick C <<<\n");
+		printf("THE LUCKY WINNER IS >>> Patrick and Brendan || Patrick C and Brendan D <<<\n");
 	}
 	if(n == 10){
-		printf("THE LUCKY WINNER IS >>> Haseeb S AND Vihan R <<<\n");
+		printf("THE LUCKY WINNER IS >>> Putchar(Pimps); || Edmund O And Andrew M <<<\n");
 	}
 	if(n == 11){
-		printf("THE LUCKY WINNER IS >>> Tristan B AND Rowan S <<<\n");
+		printf("THE LUCKY WINNER IS >>> ?* || Yixuan C And Jiaying Y <<<\n");
 	}
 	if(n == 12){
-		printf("THE LUCKY WINNER IS >>> Marc R AND Edmund O <<<\n");
+		printf("THE LUCKY WINNER IS >>> ?* || Louis Y And Vihan R <<<\n");
 	}
     return EXIT_SUCCESS;
 }
